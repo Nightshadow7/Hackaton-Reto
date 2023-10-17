affdfc16e6ffe8acac258dd41a53ba8e4f76ae58
mcamaya <camiamayar@gmail.com>
2023-10-17 18:57:31 -0500

    feat: :sparkles: Base controller usuarios

--- backend/models/Usuarios.js
+++ backend/models/Usuarios.js
@@ -1,46 +1,65 @@
 import { Schema, model } from "mongoose";
+import bcryptjs from "bcryptjs";
 
-const UserSchema = new Schema({
-  nombre: {
-    type: String,
-    required: true,
-    trim: true
+const UserSchema = new Schema(
+  {
+    nombre: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    correo: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    password: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    numeroDocumento: {
+      type: Number,
+      required: true,
+    },
+    tipoDocumento: {
+      type: String,
+      default: "Cédula de Ciudadanía",
+    },
+    cuentasAhorro: [
+      {
+        numeroCuenta: String,
+        pin: Number,
+        saldo: Number,
+        movimientos: [
+          {
+            fecha: Date,
+            monto: Number,
+            tipo: {
+              enum: ["INGRESO", "EGRESO"],
+              type: String,
+            },
+          },
+        ],
+      },
+    ],
+    estado: {
+      type: Boolean,
+      default: true,
+    },
   },
-  correo: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  password: {
-    type: String,
-    required: true,
-    trim: true
-  },
-  numeroDocumento: {
-    type: Number,
-    required: true
-  },
-  tipoDocumento: {
-    type: String,
-    default: 'Cédula de Ciudadanía'
-  },
-  cuentasAhorro: [{
-    numeroCuenta: String,
-    pin: Number,
-    saldo: Number,
-    movimientos: [{
-      fecha: Date,
-      monto: Number,
-      tipo: {
-        enum: ['INGRESO', 'EGRESO'],
-        type: String
-      }
-    }]
-  }],
-  estado: {
-    type: Boolean,
-    default: true
+  {
+    versionKey: false,
   }
-});
+);
 
-export default model('usuarios', UserSchema);
+UserSchema.statics.encryptPassword = (password) => {
+  const salt = bcryptjs.genSaltSync(10);
+  return bcryptjs.hashSync(password, salt);
+};
+
+UserSchema.statics.comparePassword = (password, receivedPassword) => {
+  return bcryptjs.compareSync(password, receivedPassword);
+};
+
+export default model("usuarios", UserSchema);
