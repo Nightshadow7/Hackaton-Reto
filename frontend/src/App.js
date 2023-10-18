8fdca265e186d0736535dc91feb5b20439ebe323
campusricardo <campusricardo2023@gmail.com>
2023-10-17 23:25:00 -0500

    style: :fire:

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import './App.css';
-import UserN_Cuenta from './Components/UserN-Cuenta';
+import UserN_Cuenta from './components/UserN-Cuenta';
 
 function App() {
   return (
