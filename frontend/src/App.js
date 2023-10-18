c3f2a5b3eb34a47ebd5bf90f0066cc9bf068c3d9
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-10-18 02:19:31 -0500

    feat: :sparkles: CREACION DE COMPONENTE USUARIO
    creacion resposiva del usuario con la navegacion a home respectiva y validacion de valores vacios del usuario

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
-import UserN_Cuenta from './components/UserN-Cuenta';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import UserN_Cuenta from './components/UserN-Cuenta';
+import Ingreso from './components/Ingreso.jsx';
+import "./app.css"
+
 
 function App() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="/UserCuenta" element={<UserN_Cuenta/>}/>
+        <Route path="/UserCuenta" element={<UserN_Cuenta />} />
+        <Route path="/Ingreso" element={<Ingreso />} />
       </Routes>
     </BrowserRouter>
   );
