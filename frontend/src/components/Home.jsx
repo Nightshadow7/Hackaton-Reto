a30daa90f5e5cbaeec4d7283076eec5e5fd87a5f
JuanDavidEscalanteCastañeda-Campus <juandaesca5@hotmail.com>
2023-10-18 11:36:40 -0500

    feat: :sparkles: CREACION DE PAGINA HOME Y CORRECCION DE DEPENCIA DEL PACKAGE.JSON
    se ha creado la pagina home responsive y se ah corregido una mala instalacion de la dependencia del proyecto

--- frontend/src/components/Home.jsx
+++ frontend/src/components/Home.jsx
@@ -1,11 +1,18 @@
 import "./home.css"
 import Header from "./sub-componets/Header"
+import image from "../resources/economia_empresa_hero-1024x824.png"
 
 function Home() {
     return ( 
         <div className="displayHome">
-            <div>
             <Header/>
+            <div className="body">
+                <div className="bienvenido">
+                <h1 className="welcome">Bienvenidos</h1>
+                </div>
+                <div className="contImg">
+                <img src={image} className="bot"/>
+                </div>
             </div>
         </div>
      );
