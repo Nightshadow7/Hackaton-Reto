17df844aa8bdc36b0380a2129a9f0f3aa45c1f48
edwnig17 <edwingstiven20@gmail.com>
2023-10-18 11:54:59 -0500

    Arreglado sólo falta el css del qr

--- frontend/src/components/pay.jsx
+++ frontend/src/components/pay.jsx
@@ -1,50 +1,59 @@
 import React, { useEffect, useState } from "react";
 import { Html5QrcodeScanner } from "html5-qrcode";
+import "./Pay.css"; 
 
 const Pay = () => {
   const [scanResult, setScanResult] = useState(null);
 
-  useEffect(() => {
-    const startScanner = async () => {
-      const scanner = new Html5QrcodeScanner("reader", {
-        qrbox: {
-          width: 500,
-          height: 500,
-        },
-        fps: 2,
-      });
+  const startScanner = async () => {
+    const scanner = new Html5QrcodeScanner("reader", {
+      qrbox: {
+        width: 500,
+        height: 500,
+      },
+      fps: 2,
+    });
 
-      const success = (result) => {
-        setScanResult(result);
-        alert(result);
-        scanner.clear(); // Detener el escaneo después de encontrar un código QR
-      };
-
-      const error = (err) => {
-        console.warn(err);
-      };
-
-      try {
-        await scanner.render(success, error);
-      } catch (e) {
-        console.error(e);
-      }
+    const success = (result) => {
+      setScanResult(result);
+      scanner.clear(); // Detener el escaneo después de encontrar un código QR
     };
 
+    const error = (err) => {
+      console.warn(err);
+    };
+
+    try {
+      await scanner.render(success, error);
+    } catch (e) {
+      console.error(e);
+    }
+  };
+
+  const resetScanner = () => {
+    setScanResult(null);
+    startScanner(); // Reinicia el escáner
+  };
+
+  useEffect(() => {
     startScanner();
   }, []);
 
   return (
-    <>
+    <div className="container">
       <h1>Escanea el código QR para hacer pagos</h1>
+      <div id="reader"> </div>
       {scanResult ? (
-        <div>
+        <div className="result">
           Success: <a href={"https://" + scanResult}>{scanResult}</a>
         </div>
-      ) : (
-        <div id="reader"> </div>
+      ) : null}
+      {scanResult && (
+        <button className="reset-button" onClick={resetScanner}>
+          Reiniciar
+        </button>
       )}
-    </>
+    </div>
   );
 };
 
