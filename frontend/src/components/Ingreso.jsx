2807bae756a025dda51512b093397b0b9cc54b32
edwnig17 <edwingstiven20@gmail.com>
2023-10-18 14:14:21 -0500

    Ya sirve el login registrado

--- frontend/src/components/Ingreso.jsx
+++ frontend/src/components/Ingreso.jsx
@@ -1,113 +1,116 @@
 import { useNavigate } from "react-router-dom";
 import { useState } from "react";
-import "./ingreso.css"
-import logoOne from "../resources/logoComultrasan.png"
-import logoTwo from "../resources/financiera_comultrasan.png"
-import swal from 'sweetalert2/src/sweetalert2.js';
+import "./ingreso.css";
+import logoOne from "../resources/logoComultrasan.png";
+import logoTwo from "../resources/financiera_comultrasan.png";
+import swal from "sweetalert2/src/sweetalert2.js";
 import "sweetalert2/dist/sweetalert2.css";
+import axios from "axios";
 
 function Ingreso() {
+  const [correo, setCorreo] = useState("");
+  const [password, setPassword] = useState("");
+  const navigate = useNavigate();
 
-    const [user, setUser] = useState('');
-    const [password, setPassword] = useState('');
-    const navigate = useNavigate()
+  const InputChange = (event) => {
+    const target = event.target;
+    const value = target.value;
+    const name = target.name;
 
-    const InputChange = (event) => {
-        const target = event.target;
-        const value = target.value;
-        const name = target.name;
-
-        if (name === 'user') {
-            setUser(value);
-        } else if (name === 'password') {
-            setPassword(value);
-        }
-    };
-
-    const onSubmit = (event) => {
-        event.preventDefault();
-
-        if (password !== "") {
-
-            if (user !== "") {
-
-                const dataJSON = {
-                    "user": user,
-                    "password": password
-                }
-            navigate('/home')
-                console.log(dataJSON)
-            } else {
-                new swal({
-                    icon: 'error',
-                    title: 'Datos vacios',
-                    text: 'porfavor llena todos los datos'
-                });
-            }
-
-        } else {
-
-            new swal({
-                icon: 'error',
-                title: 'Datos vacios',
-                text: 'porfavor llena todos los datos'
-            });
-        }
-
+    if (name === "correo") {
+      setCorreo(value);
+    } else if (name === "password") {
+      setPassword(value);
     }
+  };
+  const onSubmit = async (event) => {
+    event.preventDefault();
+  
+    if (!correo.trim() || !password.trim()) {
+      new swal({
+        icon: "error",
+        title: "Datos vacíos",
+        text: "Por favor llena todos los datos",
+      });
+      return;
+    }
+  
+    try {
+      const response = await axios.post("http://localhost:7000/api/auth", {
+         correo,  
+         password,
+      });
+      console.log(response.data);
+      if (response.data.usuario) {
+        navigate("/home");
+      } else {
+        new swal({
+          icon: "error",
+          title: "Error de autenticación",
+          text: "Usuario o contraseña incorrectos",
+        });
+      }
+    } catch (error) {
+      console.error(error);
+      new swal({
+        icon: "error",
+        title: "Error de autenticación",
+        text: "Ocurrió un error al autenticar. Inténtalo de nuevo.",
+      });
+    }
+  };
+  
 
+  const NavigateHome = () => {
+    navigate("/");
+  };
 
-
-
-
-
-const NavigateHome = () => {
-    navigate("/")
-}
-
-return (
+  return (
     <>
-        <div className="natvar">
-            <div className="logo">
-                <img src={logoOne} alt="#logo" className="logoImgOne" />
-                <img src={logoTwo} alt="#logo" className="logoImgTwo" />
-            </div>
-            <div className="exit">
-                <h1 className="x" onClick={NavigateHome}>X</h1>
-            </div>
+      <div className="natvar">
+        <div className="logo">
+          <img src={logoOne} alt="#logo" className="logoImgOne" />
+          <img src={logoTwo} alt="#logo" className="logoImgTwo" />
         </div>
-        <div className="display">
-            <div className="border">
-                <div className="contain">
-                    <form className="block" onSubmit={onSubmit}>
-                        <h1 className="Ingreso">INGRESO</h1>
-                        <p className="subTitle">usuario</p>
-                        <input
-                            type="text"
-                            className="Input"
-                            onChange={InputChange}
-                            value={user}
-                            name="user"
-                        />
-                        <p className="subTitle">contraseña</p>
-                        <input
-                            type="password"
-                            className="Input"
-                            onChange={InputChange}
-                            value={password}
-                            name="password"
-                        />
-
-                        <div className="blockButtom">
-                            <button type="submit"  className="submit">Ingresar</button>
-                        </div>
-                    </form>
-
-                </div>
-            </div>
+        <div className="exit">
+          <h1 className="x" onClick={NavigateHome}>
+            X
+          </h1>
         </div>
+      </div>
+      <div className="display">
+        <div className="border">
+          <div className="contain">
+            <form className="block" onSubmit={onSubmit}>
+              <h1 className="Ingreso">INGRESO</h1>
+              <p className="subTitle">Correo</p> 
+              <input
+                type="text"
+                className="Input"
+                onChange={InputChange}
+                value={correo}
+                name="correo" 
+              />
+              <p className="subTitle">Contraseña</p> 
+              <input
+                type="password"
+                className="Input"
+                onChange={InputChange}
+                value={password}
+                name="password"
+              />
+
+              <div className="blockButtom">
+                <button type="submit" className="submit">
+                  Ingresar
+                </button>
+              </div>
+            </form>
+          </div>
+        </div>
+      </div>
     </>
-);
+  );
 }
 
 export default Ingreso;
