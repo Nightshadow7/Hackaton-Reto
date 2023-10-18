47a91751a72012a3a805aaaa5b22aa3d0a6638e9
edwnig17 <edwingstiven20@gmail.com>
2023-10-18 11:37:45 -0500

    Ya está el code escaneado y todo

--- frontend/src/components/pay.jsx
+++ frontend/src/components/pay.jsx
@@ -1,49 +1,51 @@
-import React, {useEffect, useState} from "react";
-import {Html5QrcodeScanner} from "html5-qrcode";
+import React, { useEffect, useState } from "react";
+import { Html5QrcodeScanner } from "html5-qrcode";
 
 const Pay = () => {
+  const [scanResult, setScanResult] = useState(null);
 
-    const { scanResult, setScanResult} = useState(null);
+  useEffect(() => {
+    const startScanner = async () => {
+      const scanner = new Html5QrcodeScanner("reader", {
+        qrbox: {
+          width: 500,
+          height: 500,
+        },
+        fps: 2,
+      });
 
-    useEffect(()=> {
-        const scanner = new Html5QrcodeScanner('reader', {
-            qrbox: {
-                width: 500,
-                height: 500,
-            },
-            fps: 5
-        });
-    
-        scanner.render(success, error);
-    
-        function success(result){
-           scanner.clear();
-           setScanResult(result);
-        };
-        
-        function error(err){
-            console.warn(err);
-    
-    
-        };
+      const success = (result) => {
+        setScanResult(result);
+        alert(result);
+        scanner.clear(); // Detener el escaneo después de encontrar un código QR
+      };
 
-    }, []);
+      const error = (err) => {
+        console.warn(err);
+      };
 
+      try {
+        await scanner.render(success, error);
+      } catch (e) {
+        console.error(e);
+      }
+    };
 
-    return (
-        <>
-        <h1> Escanea el codigo qr para hacer pagos</h1>
-        { scanResult
-            ? <div> Success: <a href={"https://"+scanResult}> {scanResult} </a></div>
-            : <div id="reader"> </div>
-        
-        }
+    startScanner();
+  }, []);
 
-        </>
-    );
-
-
+  return (
+    <>
+      <h1>Escanea el código QR para hacer pagos</h1>
+      {scanResult ? (
+        <div>
+          Success: <a href={"https://" + scanResult}>{scanResult}</a>
+        </div>
+      ) : (
+        <div id="reader"> </div>
+      )}
+    </>
+  );
 };
-
 
 export default Pay;
