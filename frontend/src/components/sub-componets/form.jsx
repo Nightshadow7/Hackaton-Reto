666bcb72a50aa53397ce2512f64c70054e580463
campusricardo <campusricardo2023@gmail.com>
2023-10-19 03:39:18 -0500

    feat: :sparkles: formulario funcionando

--- frontend/src/components/sub-componets/form.jsx
+++ frontend/src/components/sub-componets/form.jsx
@@ -1,21 +1,46 @@
-import React from "react";
+import React, { useState, useEffect} from "react";
 import { useLocation } from 'react-router-dom';
 const Form = () => {    
     let {state} = useLocation();
-    console.log(state);
+    const [datos, setDatos] = useState({});
+
+    // datos que vamos a traer por defecto para que las cosas no se bugueen
+    useEffect(()=> {
+        const defaultData = state.documento.campos.map((e)=>{ return [[e.titulo], '']});
+    // transforma la array en un objeto
+        const objData = Object.fromEntries(defaultData);
+        setDatos(objData);
+        console.log(objData);
+        console.log(datos);
+    }, [state]); 
+    
+
+    const inputsHanlder = (e) => {
+        const value = e.target.type === 'number' ? Number(e.target.value) : e.target.value;
+        const key = e.target.id;
+        setDatos((prevValues)=> {
+            return {...prevValues, [key]: value};
+        });
+    };
+
+    const createQR = (e) => {
+        console.log(e);
+    };
+
     return (
         <>
-        <form>
+        <div>
             <label>{state.documento.nombre}</label>
-            {state.documento.campos.map((e)=> (
-                <>
+            {state.documento.campos.map((e)=> {
+               return( <>
                 <label>{e.titulo}</label>
-                <input type={e.tipo} />
-                </>
-            ))
+                <input onChange={inputsHanlder} id={e.titulo} value={datos[[e.titulo]]} type={e.tipo} />
+                </>);
+                })
 
             }
-        </form>
+            <button type="submit"> Crear QR Code </button>
+        </div>
         </>
     );
 };
